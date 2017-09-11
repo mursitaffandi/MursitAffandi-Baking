e8650d9cdd4c28171ff0e26888ccb7c400038b84
mursyed <mursitaffandi@gmail.com>
2017-09-11 21:37:47 +0700

    Initial commit

---
+++ app/src/main/java/com/mursitaffandi/mursitaffandi_baking/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.mursitaffandi.mursitaffandi_baking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
